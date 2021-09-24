bd713edaabcf7dc6a96c42aedbf4d06c094d2f3b
Willian Dagostin Bittencourt <85398644+williandb@users.noreply.github.com>
2021-09-24 14:52:39 -0300

    Update ConnectionManager.java

--- src/util/ConnectionManager.java
+++ src/util/ConnectionManager.java
@@ -9,7 +9,7 @@ public class ConnectionManager {
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
         Class.forName("org.postgresql.Driver");
         String url = "jdbc:postgresql://localhost:5432/betha";
-        Connection conn = DriverManager.getConnection(url, "postgres", "w1ll1@n.8409");
+        Connection conn = DriverManager.getConnection(url, "postgres", "123456");
         return conn;
     }
 
